250a4d54b7bf21204e59cf179c7530e872730152
gygguguguguygyugu <168925018+gygguguguguygyugu@users.noreply.github.com>
2024-09-10 17:29:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen'; // Correct path to HomeScreen
+
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: '' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
